7158c3f04c6b79465b6de0bd1412a80ca5290904
Oleg Chernovskiy <adonai@xaker.ru>
2018-04-16 09:39:45 +0300

    Actually fix column count detection and contents

--- src/tables.rs
+++ src/tables.rs
@@ -35,13 +35,13 @@ impl TagHandler for TableHandler {
         let mut rows = find_children(tag, "tr");
         {
             // detect row count
-            let most_big_row = rows.iter().max_by(|left, right| left.children.borrow().len().cmp(&right.children.borrow().len()));
+            let most_big_row = rows.iter().max_by(|left, right| collect_children(&left, "td").len().cmp(&collect_children(&right, "td").len()));
             if most_big_row.is_none() {
                 // we don't have rows with content at all
                 return;
             }
             // have rows with content, set column count
-            self.column_count = most_big_row.unwrap().children.borrow().len();
+            self.column_count = collect_children(&most_big_row.unwrap(), "td").len();
         }
 
         // add header row
@@ -55,10 +55,11 @@ impl TagHandler for TableHandler {
         rows.retain(|row| { let children = row.children.borrow(); return children.iter().any(|child| tag_name(&child) == "td"); });
         for row in &rows {
             table_markup.push('|');
-            let cells = row.children.borrow();
+            let cells = collect_children(row, "td");
             for index in 0..self.column_count { // we need to fill all cells in a column, even if some rows don't have enough
                 if let Some(cell) = cells.get(index) {
                     // have a cell here, fill with its content 
+                    println!("{}", tag_name(&cell));
                     let text = to_text(cell);
                     if !text.is_empty() {
                         table_markup.push_str(&to_text(cell));
@@ -94,6 +95,8 @@ fn tag_name(tag: &Handle) -> String {
     }
 }
 
+/// Find descendants of this tag with tag name `name`
+/// This includes both direct children and descendants
 fn  find_children(tag: &Handle, name: &str) -> Vec<Handle> {
     let mut result: Vec<Handle> = vec![];
     let children = tag.children.borrow();
@@ -109,6 +112,22 @@ fn  find_children(tag: &Handle, name: &str) -> Vec<Handle> {
     return result;
 }
 
+/// Collect direct children with same name
+/// This doesn't include descendants
+fn collect_children(tag: &Handle, name: &str) -> Vec<Handle> {
+    let mut result: Vec<Handle> = vec![];
+    let children = tag.children.borrow();
+    for child in children.iter() {
+        if tag_name(&child) == name {
+            result.push(child.clone());
+        }
+    }
+
+    return result;
+}
+
+/// Convert html tag to text. This collects all tag children in correct order where they're observed
+/// and concatenates their text, recursively.
 fn  to_text(tag: &Handle) -> String {
     let mut result = String::new();
     match tag.data {
