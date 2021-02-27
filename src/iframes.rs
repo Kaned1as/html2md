9b6c4d2e5fd87b5fd6fa94dce10d1cb63d0bf921
Oleg Chernovskiy <kanedias@keemail.me>
2021-02-27 22:58:30 +0300

    Add vk.com iframe sample

--- src/iframes.rs
+++ src/iframes.rs
@@ -10,18 +10,24 @@ use regex::Regex;
 use markup5ever_rcdom::Handle;
 
 lazy_static! {
-    /// Pattern that detects iframes with Youtube embedded videos
+    /// Pattern that detects iframes with Youtube embedded videos<br/>
     /// Examples:
     /// * `https://www.youtube.com/embed/zE-dmXZp3nU?wmode=opaque`
     /// * `https://www.youtube-nocookie.com/embed/5yo6exIypkY`
     /// * `https://www.youtube.com/embed/TXm6IXrbQuM`
     static ref YOUTUBE_PATTERN : Regex = Regex::new(r"www\.youtube(?:-nocookie)?\.com/embed/([-\w]+)").unwrap();
 
-    /// Pattern that detects iframes with Instagram embedded photos
+    /// Pattern that detects iframes with Instagram embedded photos<br/>
     /// Examples:
     /// * `https://www.instagram.com/p/B1BKr9Wo8YX/embed/`
     /// * `https://www.instagram.com/p/BpKjlo-B4uI/embed/`
     static ref INSTAGRAM_PATTERN: Regex = Regex::new(r"www\.instagram\.com/p/([-\w]+)/embed").unwrap();
+
+    /// Patter that detects iframes with VKontakte embedded videos<br/>
+    /// Examples:
+    /// * `https://vk.com/video_ext.php?oid=-49423435&id=456245092&hash=e1611aefe899c4f8`
+    /// * `https://vk.com/video_ext.php?oid=-76477496&id=456239454&hash=ebfdc2d386617b97`
+    static ref VK_PATTERN: Regex = Regex::new(r"vk\.com/video_ext\.php\?oid=(-?\d+)&id=(\d+)&hash=(.*)").unwrap();
 
     static ref YANDEX_MUSIC_TRACK_PATTERN: Regex = Regex::new(r"https://music.yandex.ru/iframe/#track/(\d+)/(\d+)").unwrap();
     static ref YANDEX_MUSIC_ALBUM_PATTERN: Regex = Regex::new(r"https://music.yandex.ru/iframe/#album/(\d+)").unwrap();
@@ -56,6 +62,14 @@ impl TagHandler for IframeHandler {
         if let Some(capture) = INSTAGRAM_PATTERN.captures(&src) {
             let media_id = capture.get(1).map_or("", |m| m.as_str());
             printer.append_str(&format!("[![Embedded Instagram post](https://www.instagram.com/p/{mid}/media/?size=m)](https://www.instagram.com/p/{mid}/embed/)", mid = media_id));
+            return
+        }
+
+        if let Some(capture) = VK_PATTERN.captures(&src) {
+            let owner_id = capture.get(1).map_or("", |m| m.as_str());
+            let video_id = capture.get(2).map_or("", |m| m.as_str());
+            let _hash = capture.get(3).map_or("", |m| m.as_str());
+            printer.append_str(&format!("[![Embedded VK video](https://st.vk.com/images/icons/video_empty_2x.png)](https://vk.com/video{oid}_{vid})", oid = owner_id, vid = video_id));
             return
         }
 
